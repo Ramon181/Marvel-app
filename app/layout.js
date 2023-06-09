63f978e225cc4320577de6d233aa9b896d83bd28
Ramon181 <ramonantonio29970476@gmail.com>
2023-06-09 18:54:27 -0500

    paginacion lista

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Navigation } from "./components/Navigation.jsx";
+import { Navigation } from "./components/Navigation/Navigation.jsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
